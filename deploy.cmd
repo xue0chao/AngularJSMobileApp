40e6216670527ff858e977bdcb24a4fac2bb014d
Chao Xue <xuechao@alum.mit.edu>
2014-12-29 01:10:09 -0800

    Changed deployment file comments

--- deploy.cmd
+++ deploy.cmd
@@ -105,7 +105,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-# 4. Install bower packages
+:: 4. Install bower packages
 IF EXIST "%DEPLOYMENT_TARGET/bower.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install bower
